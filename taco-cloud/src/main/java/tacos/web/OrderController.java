4c0e3163ff9850b7251dd7b2387e3ad0fbf28775
doyuni <gdypc@naver.com>
2020-06-14 10:53:56 +0900

    refactor: Refactor from JDBC to JPA

--- taco-cloud/src/main/java/tacos/web/OrderController.java
+++ taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -1,8 +1,8 @@
 package tacos.web;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,12 +22,14 @@ public class OrderController {
 
     private OrderRepository orderRepository;
 
+    @Autowired
     public OrderController(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
     }
 
     @GetMapping("/current")
     public String orderForm() {
+
         return "orderForm";
     }
 
@@ -36,6 +38,7 @@ public class OrderController {
         if (errors.hasErrors()) {
             return "orderForm";
         }
+
         orderRepository.save(order);
         sessionStatus.setComplete();
 
